516b8edc28a01509403ec87f714053e198b5807c
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-08-15 19:51:26 -0400

    Add more info to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815232709) do
+ActiveRecord::Schema.define(version: 20160815235013) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
